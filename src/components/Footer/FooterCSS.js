3eac9c714121a80a9c4cce0ea59952a34cea2cb4
Anderson F. Ribeiro Campos <andersonspl@hotmail.com>
2025-01-23 14:31:22 -0300

    add: adicionando estilo na seção footer

--- src/components/Footer/FooterCSS.js
+++ src/components/Footer/FooterCSS.js
@@ -2,7 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     display: flex;
-    background-color: var(--color-black)
+    justify-content: center;
+
+    padding-block: .6rem;
+    background-color: var(--color-black);
+    color: var(--color-ice);
 
     .footer-content{
         display: flex;
