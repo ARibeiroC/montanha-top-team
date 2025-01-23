8bbc61bc231feefe49553470734218da70dd5929
Anderson F. Ribeiro Campos <andersonspl@hotmail.com>
2025-01-23 14:23:52 -0300

    fix: correção no tamanho dos links do menu

--- src/components/Menu/Nav/NavCSS.js
+++ src/components/Menu/Nav/NavCSS.js
@@ -4,10 +4,11 @@ export const Container = styled.nav`
     display: flex;
     justify-content: center;
     a {
-        width: 120px;
+        width: 150px;
         border: 1px solid transparent;
         color: var(--color-ice);
         font-weight: bold;
+        font-size: 1.2rem;
         text-align: center;
         padding: .6rem 0;
         transition: .4s;
