a0be07289e916d60fb993f3675af31465858221f
ARibeiroC <andersonspl@hotmail.com>
2025-11-20 01:13:37 -0300

    Fix: Altura da tela de cadastro atualizada para 84vh

--- src/routes/Register/RegisterCSS.js
+++ src/routes/Register/RegisterCSS.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import backgroundImage from '../../assets/LOGO_VETORIZADA_COM_FUNDO_BRANCO_800x275.png';
 
 export const Container = styled.div`
-  height: 88vh;
+  height: 84vh;
 
   background-color: rgba(0,0,0, .9);
   background-image: url(${backgroundImage});
