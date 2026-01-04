53d316731c874a424ade7a1450c3fde8d7ac18ab
ARibeiroC <andersonspl@hotmail.com>
2026-01-04 17:51:50 -0300

    nivel de permissão do admin

--- src/repository/mock.js
+++ src/repository/mock.js
@@ -3,7 +3,7 @@
 
 // TABLE: USERS (Login)
 export const mockUsers = [
-    { id: 'admin', name: 'Administrador', email: 'admin@montanha.com', role: 'admin', accessLevel: 4, branch: 'Montanha Top Team', password: 'admin' },
+    { id: 'admin', name: 'Administrador', email: 'admin@montanha.com', role: 'admin', accessLevel: 3, branch: 'Montanha Top Team', password: 'admin' },
     { id: 'teacher', name: 'Professor Mestre', email: 'professor@montanha.com', role: 'teacher', accessLevel: 1, branch: 'Montanha Top Team', password: '123' },
     { id: 'prof-test', name: 'Professor Teste', email: 'prof@montanha.com', role: 'teacher', accessLevel: 1, branch: 'Montanha Top Team', password: '123' },
     { id: 'guardian', name: 'Responsável Silva', email: 'responsavel@montanha.com', role: 'guardian', accessLevel: 0, branch: 'Montanha Top Team', password: '123' },
